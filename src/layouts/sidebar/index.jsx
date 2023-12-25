9248add35550cbcf59df64604864744998ab864d
batuhan <batuhan_kanra_60@hotmail.com>
2023-12-25 22:13:03 +0300

    sidebar-bug complate , fetch

--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -6,10 +6,10 @@ import Account from './account'
 export default function SideBar() {
   
   return (
-    <aside className='w-[275px] min-h-screen max-h-screen px-2 flex flex-col overflow-auto '>
-        <Logo />
-        <Menu />
-        <Account />
-    </aside>
+    <aside className="w-[275px] max-h-screen min-h-screen px-2 flex flex-col sticky z-[2] top-0">
+			<Logo />
+			<Menu />
+			<Account />
+		</aside>
   )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import MainLayout from '~/layouts/main'
+import { useState,useEffect } from 'react'
+
 
 export default function Home() {
+  const [api,setApi] =useState(false)
+
+
+useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(res=>res.json())
+    .then(filter=>filter.slice(1,4))
+    .then(data=>setApi(data))
+},[])
+
   return (
-    <div> home </div>
+    <div className='container mx-auto'> 
+      {api && api.map(item=>(
+        <div key={item.id}>
+          <img src={item.url} alt={item.id} />
+          {item.title}
+        </div>
+      ))}
+      
+     </div>
   )
 }
